99e9b774349ca9cb356e381ab68b1d87dd001216
ChrisCo31 <christel.coeur@free.fr>
2018-02-15 09:55:59 +0100

    Formulaire langue V1

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -9,17 +9,35 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 class BookingController extends Controller
 {
     /**
      * @return Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $content = $this->get('templating')->render('AppBundle:Booking:index.html.twig');
-        return new Response ($content);
+        // créer le formulaire de choix de la langue 
+        $form = $this->createFormBuilder()
+            ->add('langue', ChoiceType::class, array(
+                'choices' => array(
+                    'english'=>'en',
+                    'français' => 'fr',
+                )
+            ))
+            ->add('send', submitType::class, array('label'=>'Acheter mes billets en ligne'))
+            ->getForm();
+        // pour traiter les donnees du form j'appelle la methode handleRequest
+        // ouverture d'une session
+        // redirection vers la page "organize"
+
+        return $this->render('AppBundle:Booking:index.html.twig', array('form'=>$form->createView()));
     }
 
 }

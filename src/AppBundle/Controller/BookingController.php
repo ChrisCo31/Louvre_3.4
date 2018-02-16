b6f8192a5f1d22f87144ab1a0ebb3915eadf1d2b
ChrisCo31 <christel.coeur@free.fr>
2018-02-16 18:11:01 +0100

    Structure template : layout de base + views dans src

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -16,10 +16,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
-
 class BookingController extends Controller
 {
-     /**
+    /**
      * Matches /
      * @route("/", name="booking_home")
      */

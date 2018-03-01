d057e4f6ce1f87e6b4036112ca5ac97a46142c95
ChrisCo31 <christel.coeur@free.fr>
2018-03-01 11:25:24 +0100

    nettoyage

--- src/AppBundle/Form/ReservationType.php
+++ src/AppBundle/Form/ReservationType.php
@@ -18,7 +18,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Validator\Constraints as Assert;
+
 
 class ReservationType extends AbstractType
 {

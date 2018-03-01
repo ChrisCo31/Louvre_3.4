4f0b2ca5264672201576bb5e548594877aa03c4f
ChrisCo31 <christel.coeur@free.fr>
2018-03-01 11:23:37 +0100

    methode identificationAction

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -13,11 +13,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Form\ReservationType;
+use AppBundle\Form\TicketType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use AppBundle\Entity\Reservation;
+use AppBundle\Entity\Ticket;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -42,27 +44,22 @@ class BookingController extends Controller
     {
         $reservation = new Reservation();
         $form =$this->get('form.factory')->create(ReservationType :: class, $reservation);
-
         //  1. Verification que la requete est de type POST
         if($request->isMethod('POST'))
         {
             //  2. Recuperation des valeurs pour hydrater l'objet
             $form->handleRequest($request);
-
             // 3. Verification des valeurs et validation de l'objet
             if($form->isValid())
             {
                 //ouverture d'une session et on garde les infos en session
                 $reservation = $form->getData();
-                var_dump($reservation);
-
                 $this->get('session')->set('reservation', $reservation);
                 //redirection vers la page d'identification
                 return $this->redirectToRoute('booking_identification');
             }
         }
         // Creation du formulaire
-        $form = $this->get('form.factory')->create(ReservationType::class);
         return $this->render('AppBundle:Booking:organize.html.twig', ['form'=> $form->createView()]);
     }
     /**
@@ -71,21 +68,36 @@ class BookingController extends Controller
      */
     public function identificationAction(Request $request)
     {
+        //recuperation des info de la page precedente
         $reservation = $request->getSession()->get('reservation');
-
-
-        return $this->render('AppBundle:Booking:identification.html.twig', [
-            'reservation' => $reservation,
-
-        ]);
+        //formulaire ticket
+        $ticket = new Ticket();
+        $form=$this->get('form.factory')->create(TicketType::class, $ticket);
+        if($request->isMethod('POST'))
+        {
+            $form->handleRequest($request);
+            if($form->isValid())
+            {
+                $ticket = $form->getData();
+                $this->get('session')->set('ticket', $ticket);
+                return $this->redirectToRoute('booking_payment');
+            }
+        }
+        return $this->render('AppBundle:Booking:identification.html.twig',
+            ['reservation' => $reservation,
+             'form'        => $form->createView(),
+             ]
+        );
     }
     /**
      * Matches /payment
      * @route("/payment", name="booking_payment")
      */
-    public function paymentAction()
+    public function paymentAction(Request $request)
     {
-        return new Response ("la page de recap et d'appel de Stripe");
+        $reservation = $request->getSession()->get('reservation');
+        $ticket = $request->getSession()->get('ticket');
+        return $this->render('AppBundle:Booking:payment.html.twig',[
+            'reservation' => $reservation, 'ticket' => $ticket]);
     }
-
   }

4d823290c51c5838ece2900e0ee3bd1f07723096
ChrisCo31 <christel.coeur@free.fr>
2018-03-23 14:15:23 +0100

    installation pour test

---
+++ tests/AppBundle/Controller/BookingControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mishi
+ * Date: 15/03/2018
+ * Time: 15:22
+ */
+
+namespace Tests\AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BookingControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+
+    }
+}

bf969650880f9d1b1c8a0dd9c9ade4fae1d5805e
ChrisCo31 <christel.coeur@free.fr>
2018-03-23 14:01:58 +0100

    Creation du service pour generer un token

---
+++ src/AppBundle/Services/GenerateToken.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mishi
+ * Date: 22/03/2018
+ * Time: 10:46
+ */
+
+namespace AppBundle\Services;
+
+
+class GenerateToken
+{
+    /**
+     * @param $length
+     * @return bool|string
+     */
+    public function random($length)
+    {
+        $alphabet = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPMLKJHGFDSQWXCVBN0123456789";
+        return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
+    }
+
+}

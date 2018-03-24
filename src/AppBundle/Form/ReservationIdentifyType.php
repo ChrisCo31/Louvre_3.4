f22d2cfb92816dc150d9865a29e1b0d79ea2a6f5
ChrisCo31 <christel.coeur@free.fr>
2018-03-24 15:50:59 +0100

    Creation d'un formulaire qui inclut le form ticket

---
+++ src/AppBundle/Form/ReservationIdentifyType.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mishi
+ * Date: 23/03/2018
+ * Time: 18:06
+ */
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ReservationIdentifyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('tickets', CollectionType::class, array(
+                    'entry_type' => TicketType::class)
+            )
+            ->add('Ajouter', SubmitType::class);
+    }
+
+}

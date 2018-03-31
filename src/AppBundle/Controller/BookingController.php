1bdbf15e0b666c895ae49c5c80936608c87d50be
ChrisCo31 <christel.coeur@free.fr>
2018-03-31 11:06:56 +0200

    delestage du controlleur par la mise en place de service

--- src/AppBundle/Controller/BookingController.php
+++ src/AppBundle/Controller/BookingController.php
@@ -32,8 +32,10 @@ class BookingController extends Controller
      */
     public function indexAction(Request $request)
     {
+
         $locale = $request->getLocale();
         return $this->render('AppBundle:Booking:index.html.twig', array('locale' =>$locale));
+
     }
 
     /**
@@ -45,51 +47,43 @@ class BookingController extends Controller
     {
         $reservation = new Reservation();
 
-
         $form =$this->createForm(ReservationType :: class, $reservation);
-
-        // recuperation du service
-        $closingDay = $this->get('app.InvalidBookingDate');
+        // recuperation du service qui va checker la date / jours de fermetures
+        $closingDay = $this->get('app.ValidateDate');
+        // recuperation du service qui va controler si le nombre de ticket max a ete vendu
         $max = $this->get('app.MaxTicketSold');
+        //recuperation du service qui controle l'heure de reservation et exclut la reservation à la journée
+        $half = $this->get('app.HalfDay');
         $dateVisit = $reservation->getDateVisit();
         $nbTicket = $reservation->getNbTicket();
-
-        if($max->MaxTicket($dateVisit, $nbTicket)) {
-            throw new \Exception('trop de tickets vendu');
-        }else
+        if($max->MaxTicket($dateVisit, $nbTicket))
         {
-            echo "good";
+            throw new \Exception('trop de tickets vendu');
         }
-
-
         //  1. Verification que la requete est de type POST
         if($request->isMethod('POST'))
         {
             //  2. Recuperation des valeurs pour hydrater l'objet
             $form->handleRequest($request);
-            $dateVisit = $reservation->getDateVisit();
-
-
-            if($closingDay->isClosing($dateVisit))
+           $dateVisit = $reservation->getDateVisit();
+            if(($closingDay->checkDay($dateVisit) == true) )
             {
-                throw new \Exception('Reservation Impossible');
-            }
-            if($closingDay->isPast($dateVisit)) {
-                throw new \Exception('la date choisi est deja passé');
-            }
-            if($closingDay->isHalfDay($reservation)) {
-                throw new \Exception('demi journee only');
-            }
+               echo "reservation impossible";
 
-
-            // 3. Verification des valeurs et validation de l'objet
-            if($form->isValid())
-            {   //ouverture d'une session et on garde les infos en session
-                $reservation = $form->getData();
-                $this->get('session')->set('reservation', $reservation);
-                //redirection vers la page d'identification
-                return $this->redirectToRoute('booking_identification');
-            }
+            } elseif ($half->todayAfternoon($reservation) == true)
+            {
+                echo "demijournee only";
+            }else
+                {
+                    // 3. Verification des valeurs et validation de l'objet
+                    if($form->isValid())
+                    {   //ouverture d'une session et on garde les infos en session
+                        $reservation = $form->getData();
+                        $this->get('session')->set('reservation', $reservation);
+                        //redirection vers la page d'identification
+                        return $this->redirectToRoute('booking_identification');
+                    }
+                }
         }
         // Creation du formulaire
         return $this->render('AppBundle:Booking:organize.html.twig', ['form'=> $form->createView()]);
@@ -100,22 +94,53 @@ class BookingController extends Controller
      */
     public function identificationAction(Request $request)
     {
+
         //recuperation des info de la page precedente
         $reservation = $request->getSession()->get('reservation');
         //formulaire ticket a remplir
         if(!$reservation->hasAllTicket()) $this->get('app.GenerateTicket')->generateTicket($reservation);
         $form = $this->createForm(ReservationIdentifyType::class, $reservation);
+        //recuperation de tous les tickets
+        $tickets = $reservation->getTickets();
+        foreach ($tickets as $ticket) {
+            $birthDate = $ticket->getbirthDate(); // on va chercher l'objet datetime
+            // on affiche la date dans l'objet datetime
+            $birthDate = $birthDate->format('y-m-d');
+            return $birthDate;
+        }
+
+
+        // appel le service PriceCalculator
+        //$price = $this->get('app.PriceCalculator');
         //formulaire ticket rempli
         if($request->isMethod('POST'))
         {
             $form->handleRequest($request);
+           // $ticket = $form->getData();
+            //var_dump($reservation);
+
+
+            $birthDate = $ticket->getbirthDate(); // j'appelle la methode getAge de la classe ticket
+            $firstName = $ticket->getFirstName();
+            //var_dump($firstName);
+            //var_dump($birthDate);
+
+            $age = $birthDate->getAge($birthDate);
+            $price->pricePerTicket($birthDate, $age);
+            $price->pricePerReservation($birthDate, $price);
+            echo $price;
+
+        }
+
+
             if($form->isValid())
             {
+
                 $ticket = $form->getData();
                 $this->get('session')->set('ticket', $ticket);
                 return $this->redirectToRoute('booking_payment');
             }
-        }
+
         return $this->render('AppBundle:Booking:identification.html.twig',
             ['reservation' => $reservation,
              'form'        => $form->createView(),

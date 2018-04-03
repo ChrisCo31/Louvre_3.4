cb127ea893ccfa1d156cb0d9cc284f0cdcabd9e8
ChrisCo31 <christel.coeur@free.fr>
2018-04-03 13:49:42 +0200

    Creation du service priceCalculator

--- src/AppBundle/Services/PriceCalculator.php
+++ src/AppBundle/Services/PriceCalculator.php
@@ -12,79 +12,33 @@ use AppBundle\Entity\Ticket;
 
 class PriceCalculator
 {
-    //  Prix afferents aux limites d'ages & reductions
 
-    const BABY_PRICE = 0;
-    const CHILD_PRICE = 8;
-    const ADULT_PRICE = 16;
-    const SENIOR_PRICE = 12;
-    const REDUCED_PRICE = 10;
-    const HALF_DAY = 0.5;
-
-    const HIGH_AGE_LIMIT_BABY = 4;
-    const HIGH_AGE_LIMIT_CHILD = 12;
-    const HIGH_AGE_LIMIT_ADULT = 59;
-    const LOW_AGE_LIMIT_SENIOR = 60;
-
+/*
     public function __construct()
     {
         $ticket = new Ticket();
         $this->ticket = $ticket;
     }
-
-    /**
-     * @param $age
-     * @return int
-     */
-    private function pricePerAge($ticket, $birthDate) // Calcul du prix en fonction de l'age
-   {
-       $age = $ticket->getAge($birthDate);
-       var_dump($age);
-
-       if($age <= self::HIGH_AGE_LIMIT_BABY)
-       {
-           return self::BABY_PRICE;
-
-       } elseif ($age <= self::HIGH_AGE_LIMIT_CHILD)
-       {
-           return self::CHILD_PRICE;
-
-       } elseif ($age <= self::HIGH_AGE_LIMIT_ADULT)
-       {
-           return self::ADULT_PRICE;
-
-       } elseif ($age >= self::LOW_AGE_LIMIT_SENIOR)
-       {
-           return self::SENIOR_PRICE;
-       }
-   }
-
-    /**
-     * @param $ticket
-     */
-    public function pricePerTicket($age, $reduction = null) // integration du discount et calcul du prix par ticket
-   {
-       /*if($ticket->getDiscount())
-       {
-           return self::REDUCED_PRICE;
-       } else
-       {*/
-           return $pricePerTicket = $this->pricePerAge($age);
-
-   }
-
+*/
     /**
      * @param Reservation $reservation
      */
-    public function pricePerReservation($age, $reservation) //calcul du prix par reservation incluant la demi journee
+    public function calculateTotalPrice($reservation) //calcul du prix par reservation incluant la demi journee
     {
-        $total = $reservation->getPriceToPay();
-        foreach($reservation->getTickets() as $ticket)
+        //recuperation de tous les tickets
+        $tickets = $reservation->getTickets();
+        $totalPrice = 0;
+        foreach ($tickets as $ticket)
         {
-            $pricePerTicket = $this->pricePerTicket($age);
-            $total += $pricePerTicket;
-            echo $total;
-            exit();
+            $birthDate = $ticket->getbirthDate(); // on va chercher l'objet datetime
+            // Calcul de l'age
+            $age = $ticket->getAge($birthDate); // on appelle la methode getAge de l'entite Ticket avec la date de naissance en parametre
+            $price = $ticket->calculatePricePerTicket($age);
+            $truc = $ticket->setPrice($price); // il serait plus judicieux d'hydrater les objets tickets avec leur prix à un autre endroit...
+            $totalPrice = $totalPrice + $price;
         }
+        $totalPrice = $reservation->setPriceToPay($totalPrice);
+        return $totalPrice;
     }
 }
+

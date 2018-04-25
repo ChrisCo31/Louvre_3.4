f9d3f08eb2fd5b15cdfca90c99ac7527c1a73276
ChrisCo31 <christel.coeur@free.fr>
2018-04-25 15:24:36 +0200

    mise en place swfit et stripe + relation entre les deux

--- src/AppBundle/Services/PayWithStripe.php
+++ src/AppBundle/Services/PayWithStripe.php
@@ -11,22 +11,45 @@ namespace AppBundle\Services;
 
 use AppBundle\Entity\Reservation;
 use AppBundle\Entity\Transaction;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
 
 class PayWithStripe
 {
-    public function useStripe(Reservation $reservation)
+    private $sendEmail;
+    public function __construct ($sendEmail)
+    {
+        $this->sendEmail = $sendEmail;
+    }
+    public function useStripe(Request $request,Reservation $reservation, Transaction $transaction)
     {
         \Stripe\Stripe::setApiKey("sk_test_qHjRSqkcpdP6N7Y8SfVPM79H");
         // recuperation du token
         $token = $_POST['stripeToken'];
         // creation du client
-        $charge =\Stripe\Charge::create(array(
-            "amount" => $reservation->getPriceToPay()*100,
-            "currency" => "eur",
-            "source" =>  $token, // obtained with Stripe.js
-            "description" =>  $reservation->getEmail()
-        ));
+            try {
+                $charge =\Stripe\Charge::create(array(
+                    "amount" => $reservation->getPriceToPay()*100,
+                    "currency" => "eur",
+                    "source" =>  $token, // obtained with Stripe.js
+                    "description" =>  $reservation->getEmail()
+                ));
+
+            } catch (Exception $e) {
+                // mettre un message d'erreur en flash
+                // faire une redirection
+                $message = $transaction->setMessage("Probleme avec la transaction");
+                $charge = \Stripe\Charge::all();
+                $idStripe = $transaction->setIdStripe($charge['data'][0]['id']);
+                return false;
+            }
+        $charge = \Stripe\Charge::all();
+        $message = $transaction->setMessage("Transaction enregistrée dans Stripe");
+        $idStripe = $transaction->setIdStripe($charge['data'][0]['id']);
+        $this->sendEmail->sendEmail($reservation);
+
     }
     public function useCharge(Transaction $transaction)
     {
@@ -52,4 +75,5 @@ class PayWithStripe
             $message = $transaction->setMessage("unknown error");
         }
     }
+
 }
--- src/AppBundle/Services/SendEmail.php
+++ src/AppBundle/Services/SendEmail.php
@@ -9,16 +9,52 @@
 namespace AppBundle\Services;
 
 
-use Swift_Mailer;
-use Swift_Message;
-use Swift_SmtpTransport;
+use AppBundle\Entity\Reservation;
+use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\Translation\Translator;
+
 
 class SendEmail
 {
-  public function createEmail()
-  {
-      echo "WTF";
-  }
+    private $mailer;
+    private $templating;
+    private $mailer_address;
+
+    public function __construct(\Swift_Mailer $mailer, EngineInterface $templating, $translator)
+    {
+        $this->mailer = $mailer;
+        $this->templating = $templating;
+        $this->translator = $translator;
+
+    }
+    public function sendEmail (Reservation $reservation)
+    {
+        $reservation->getToken();
+        $reservation->getDateReservation();
+        $reservation->getnbTicket();
+        $reservation->getpriceToPay();
+        $subject = $this->translator->trans('Mail.Subject');
+        $from = 'coeuranger.pastel@gmail.com';
+        $to = $reservation->getEmail();
+        $format = 'text/html';
+        $body = $this->templating->render(
+            'AppBundle:Emails:Registration.html.twig', array(
+                'reservation'=>$reservation,
+                'tickets' =>$reservation->getTickets()
+            ));
+        $this->createEmail($subject, $from, $to, $format, $body);
+
+    }
+    public function createEmail($subject, $from, $to, $format, $body)
+    {
+         $message = \Swift_Message::newInstance()
+              ->setSubject($subject)
+              ->setFrom($from)
+              ->setTo($to)
+              ->setContentType($format)
+              ->setBody($body);
+         $this->mailer->send($message);
+    }
 
 
 }
